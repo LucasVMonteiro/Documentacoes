b162ac2d992460bf5d2bb9a959dea5303e7e7151
Lucas0x7CE <59663614+Lucas0x7CE@users.noreply.github.com>
2020-01-10 17:41:38 -0300

    Update exercicio-dna.c

--- exercicio-dna.c
+++ exercicio-dna.c
@@ -10,44 +10,30 @@ e que nos indices impares, seja G ou T de acordo com o indice par anterior
 se o indice par for A, entao o indice impar devera ser T
 se o indice par for C, entao o indice impar devera ser G
 
-
 */
 int main(void) {
-
+  //insira uma string composta das letras A C T G
   char dna[]= "ATCGATATCGCGATC";
   int len_str = strlen(dna);
-  printf("%d\n",len_str);
-  char gen;
   int resto ,flag, i;
   flag=0;
+  //Verificando se o primeiro indice é composto de A ou C
   if(dna[0] == 'A' || dna[0] == 'C'){
-
-    for(i=0;i<len_str;i += 2){
-      printf("%d\n",i);
-      // se i for par: verficiar se é A ou C,
-      // e verificar se o indice i+1 é G ou T
-      if(i == len_str){
-            printf("8\n");
-            break;
-      }
-      else if (dna[i] == 'A' && dna[i+1] == 'T'){
-        //se a posicao impar for verdadeira continue
+    for(i=0;i<len_str;i += 2){//iniciando iteracao a partir de 0, e ate o tamanho da string, iterando de 2 em 2 para obter indices pares
+      if (dna[i] == 'A' && dna[i+1] == 'T'){//se o indice par for A e o indice seguinte(impar) for T, continue
         continue;
-      }else if(dna[i] == 'C' && dna[i+1] == 'G'){
+      }else if(dna[i] == 'C' && dna[i+1] == 'G'){//se o indice par for C e o indice seguinte(impar) for G, continue
         continue;
-      }else{
+      }else{//caso o indice par nao seja A ou C, adicione +1 a sentinela 'flag', e saia do laço for
         flag=1;
         printf("\n ERR %d | %c \n",i,dna[i]);
         break;
       }
-    }
-    //bloco if
+    }//aqui sera analisada a sentinela, caso 0 entao a sequencia respeita os criterios, caso 1 entao esta incorreta
     if(flag==1)
       printf("SEQUENCIA INCORRETA\n");
     else
       printf("SEQUENCIA CORRETA\n");
-  
-  }   
-  
+  }
   return 0;
 }
